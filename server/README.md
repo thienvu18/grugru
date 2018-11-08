5df20b4b72123f97546192d4e2b9265bd44e8f5c
Thái Thiên Vũ <thienvu1802@gmail.com>
2018-11-08 09:36:17 +0700

    Thêm file README cho server

---
+++ server/README.md
@@ -0,0 +1,8 @@
+1. Chạy npm install
+2. Tạo 1 file tên .env với nội dung:
+    PORT=8080
+    DB_USER='Admin'
+    DB_PASSWORD='Admin'
+    SERVER_ADDRESS='localhost'
+    DB_NAME='test'
+và chỉnh sửa các giá trị cho phù hợp với server cài trên máy
--- server/server.js
+++ server/server.js
@@ -2,78 +2,80 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var sql = require("mssql");
-var app = express(); 
+var app = express();
+
+require('dotenv').config();
 
 // Body Parser Middleware
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 //CORS Middleware
 app.use(function (req, res, next) {
-    //Enabling CORS 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-    next();
+  //Enabling CORS 
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+  next();
 });
 
 //Setting up server
- var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
- });
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
 
 //Initiallising connection string
 var dbConfig = {
-    user:  '<dbUserName>',
-    password: '<dbPassword>',
-    server: '<dbHost_URL>',
-    database:' <dbName>'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.SERVER_ADDRESS,
+  database: process.env.DB_NAME,
 };
 
 //Function to connect to database and execute query
-var  executeQuery = function(res, query){             
-     sql.connect(dbConfig, function (err) {
-         if (err) {   
-                     console.log("Error while connecting database :- " + err);
-                     res.send(err);
-                  }
-                  else {
-                         // create Request object
-                         var request = new sql.Request();
-                         // query to the database
-                         request.query(query, function (err, res) {
-                           if (err) {
-                                      console.log("Error while querying database :- " + err);
-                                      res.send(err);
-                                     }
-                                     else {
-                                       res.send(res);
-                                            }
-                               });
-                       }
-      });           
+var executeQuery = function (res, query) {
+  sql.connect(dbConfig, function (err) {
+    if (err) {
+      console.log("Error while connecting database :- " + err);
+      res.send(err);
+    }
+    else {
+      // create Request object
+      var request = new sql.Request();
+      // query to the database
+      request.query(query, function (err, res) {
+        if (err) {
+          console.log("Error while querying database :- " + err);
+          res.send(err);
+        }
+        else {
+          res.send(res);
+        }
+      });
+    }
+  });
 }
 
 //GET API
-app.get("/api/user", function(req , res){
-                var query = "select * from [user]";
-                executeQuery (res, query);
+app.get("/api/user", function (req, res) {
+  var query = "select * from [user]";
+  executeQuery(res, query);
 });
 
 //POST API
- app.post("/api/user", function(req , res){
-                var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password');
-                executeQuery (res, query);
+app.post("/api/user", function (req, res) {
+  var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password)";
+  executeQuery(res, query);
 });
 
 //PUT API
- app.put("/api/user/:id", function(req , res){
-                var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
-                executeQuery (res, query);
+app.put("/api/user/:id", function (req, res) {
+  var query = "UPDATE [user] SET Name= " + req.body.Name + " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
+  executeQuery(res, query);
 });
 
 // DELETE API
- app.delete("/api/user /:id", function(req , res){
-                var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
-                executeQuery (res, query);
+app.delete("/api/user /:id", function (req, res) {
+  var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
+  executeQuery(res, query);
 });
